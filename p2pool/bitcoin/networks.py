082e7939aee8f5579c8fe757eeddf809a5cc0e3d
Jumail <mundekkat@hotmail.com>
2018-02-04 17:53:36 +1100

    Added Strayacoin config

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -364,6 +364,27 @@ nets = dict(
         DUST_THRESHOLD=0.03e8,
     ),
 
+    strayacoin=math.Object(
+        P2P_PREFIX='d0e1f5ec'.decode('hex'),
+        P2P_PORT=9666,
+        ADDRESS_VERSION=20,
+        RPC_PORT=9432,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'strayacoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=2.5 * 60, # s
+        SYMBOL='NAH',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'strayacoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/strayacoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.strayacoin'), 'strayacoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -257,6 +257,24 @@ nets = dict(
         VERSION_CHECK=lambda v: True,
     ),
 
+	strayacoin=math.Object(
+		PARENT=networks.nets['strayacoin'],
+        SHARE_PERIOD=25, # seconds target spacing
+        CHAIN_LENGTH=3*60*60//15, # shares
+        REAL_CHAIN_LENGTH=3*60*60//15, # shares
+        TARGET_LOOKBEHIND=20, # shares coinbase maturity
+        SPREAD=10, # blocks
+        IDENTIFIER='E30D71731AE64889'.decode('hex'),
+        PREFIX='B2021AF67EDF5411'.decode('hex'),
+        P2P_PORT=8444,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9444,
+        BOOTSTRAP_ADDRS=''.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+	),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
