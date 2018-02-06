d3fb3dca94f642cd19bc4594e70943d4da142efb
Jumail <mundekkat@hotmail.com>
2018-02-06 15:52:20 +1100

    Correctly rebased to p2pool-master and corrected a typo in p2pool/bitcoin/networks/strayacoin.py

--- p2pool/bitcoin/networks/strayacoin.py
+++ p2pool/bitcoin/networks/strayacoin.py
@@ -19,7 +19,7 @@ SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 2.5 * 60, # s
 SYMBOL = 'NAH'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'strayacoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/strayacoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.strayacoin'), 'strayacoin.conf'),
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'strayacoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/strayacoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.strayacoin'), 'strayacoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://straya.network/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://straya.network/address/'
 TX_EXPLORER_URL_PREFIX = 'http://straya.network/tx/'
